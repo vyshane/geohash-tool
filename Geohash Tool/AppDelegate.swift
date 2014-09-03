684ce327f5768a0c5149a02b0396378ba1b791d4
Vy-Shane Sin Fat <shane@node.mu>
2014-09-03 22:47:28 +0800

    Initial commit

---
+++ Geohash Tool/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Geohash Tool
+//
+//  Created by Vy-Shane Sin Fat on 3/09/2014.
+//  Copyright (c) 2014 Vy-Shane Sin Fat. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Geohash Tool/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Geohash Tool
+//
+//  Created by Vy-Shane Sin Fat on 3/09/2014.
+//  Copyright (c) 2014 Vy-Shane Sin Fat. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
